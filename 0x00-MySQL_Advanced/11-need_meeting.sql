a4057edd47a067a7ad1b4c1787ff752184fb9b90
keme-ebi <kelvin6bolou@gmail.com>
2024-03-22 11:03:43 +0100

    a script that creates a view

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,7 +2,7 @@
 -- under 80 and no last_meeting or more than 1 month
 DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting 
-AS SELECT name AS value FROM students
+AS SELECT name FROM students
 WHERE score < 80 AND (
 	last_meeting IS NULL
 	OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH)
