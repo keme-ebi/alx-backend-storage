dbc5fb2583b1b6c4bc2713279b06d109252030c1
keme-ebi <kelvin6bolou@gmail.com>
2024-03-28 00:37:18 +0100

    a class Cache, that store an instance of the Redis client, with a method store that generates a random key using uuid, store the input data in Redis using the random key and return the key

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+exercise
+"""
+import redis
+import uuid
+from typing import Any
+
+
+class Cache:
+    """a Cache class"""
+    def __init__(self) -> None:
+        """initialization and storing of reids client instance"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Any) -> str:
+        """generates a random key using uuid, stores the input data
+            in Redis using the random key and return the key
+        Arg:
+            data(any): can be a str, bytes, int or float
+        Return:
+            the random key generated
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
