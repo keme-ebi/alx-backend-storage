026410112b60e73bce97fff6763c70178650f853
keme-ebi <kelvin6bolou@gmail.com>
2024-03-28 09:07:45 +0100

    added a get, get_str, and get_int method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ exercise
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -26,3 +26,37 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable]
+            ) -> Union[str, bytes, int, float]:
+        """
+        gets the data stored in a key using the callable fn
+            to convert the data back to the desired format
+        Args:
+            key(str): key to get data from
+            fn: callable to convert the data back to the desired format
+        Return:
+            converted data
+        """
+        data = self._redis.get(key)
+        return fn(data) if fn else data
+
+    def get_str(self, key: str) -> str:
+        """
+        returns data from redis in str format
+        Arg:
+            key(str): key to get data from
+        Return:
+            data in str format
+        """
+        return self.get(key, lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """
+        returns data from redis in int format
+        Arg:
+            key(str): key to get data from
+        Return:
+            data in int format
+        """
+        return self.get(key, lambda d: int(d))
